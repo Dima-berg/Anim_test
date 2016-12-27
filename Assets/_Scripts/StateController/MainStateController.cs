df423c8bf09a12e429b877e52501a0d26f700a14
DESKTOP-2J8J5QE\Bridge <darkirius@gmail.com>
2016-12-27 09:31:31 +0300

    Основные баги шио. Не хватает времени описать.

---
+++ Assets/_Scripts/StateController/MainStateController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainStateController : StateMachineBehaviour
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateInfo.IsName("Hide") || stateInfo.IsName("Null"))
+        {
+            animator.SetBool("Metamorphosis", false);
+        }
+        else
+        {
+            animator.SetBool("Metamorphosis", true);
+        }
+    }
+}
---
+++ Assets/_Scripts/UnitController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    public Animator bodyAnimator;
+    public Animator soulAnimator;
+    public float MetamorphosisTimer = 0.35f;
+
+
+    void Start ()
+    {
+        bodyAnimator.Play("Show");
+        soulAnimator.Play("Null");
+    }
+	
+	public void Metamorphosis ()
+    {
+        if (bodyAnimator.GetBool("Metamorphosis"))
+        {
+            StartCoroutine("MetamorphosisSoul");
+        }
+        else
+        {
+            StartCoroutine("MetamorphosisBody");
+        }
+    }
+
+    IEnumerator MetamorphosisBody ()
+    {
+        soulAnimator.CrossFade("Hide", 0.25f);
+        yield return new WaitForSeconds(MetamorphosisTimer);
+        bodyAnimator.CrossFade("Show", 0.25f);        
+    }
+
+    IEnumerator MetamorphosisSoul()
+    {
+        bodyAnimator.CrossFade("Hide", 0.25f);
+        yield return new WaitForSeconds(MetamorphosisTimer);
+        soulAnimator.CrossFade("Show", 0.25f);
+    }
+}
