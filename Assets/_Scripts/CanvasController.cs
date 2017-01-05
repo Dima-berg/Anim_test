fa8cfb190f7d7389d87c387d672e50da488b2c32
DESKTOP-2J8J5QE\Bridge <darkirius@gmail.com>
2017-01-05 12:54:50 +0300

    Финальный контроллер для персонажа

---
+++ Assets/_Scripts/CanvasController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+    [Header("Animators")]
+    public Animator animatorBody;
+    public Animator animatorSoul;
+
+    [Header("Animator parameters")]
+    public bool animatorGround = true;
+    public bool animatorWall = false;
+    public float animatorXSpeed = 1.0f;
+    public float animatorYSpeed = 0.0f;
+    public bool animatorDead = false;
+    public bool animatorShow = true;
+    public bool animatorStartRound = false;
+    public int animatorFinish = -1;
+    public bool animatorAttack = false;
+
+    [Header("Canvas parameters")]
+    public Toggle canvasGround;
+    public Toggle canvasWall;
+    public InputField canvasXSpeed;
+    public InputField canvasYSpeed;
+    public Toggle canvasDead;
+    public Toggle canvasShow;
+    public Toggle canvasStartRound;
+    public InputField canvasFinish;
+    public Toggle canvasAttack;
+
+    void Start ()
+    {
+        SetAnimatorParametrs();
+        SetCanvasParametrs();
+    }
+	
+	void Update ()
+    {
+        SetAnimatorParametrs();
+        SetCanvasParametrs();
+    }
+
+    public void SetParametrs()
+    {
+        animatorGround = canvasGround.isOn;
+        animatorWall = canvasWall.isOn;
+        animatorXSpeed = float.Parse(canvasXSpeed.text);
+        animatorYSpeed = float.Parse(canvasYSpeed.text);
+        animatorDead = canvasDead.isOn;
+        animatorShow = canvasShow.isOn;
+        animatorStartRound = canvasStartRound.isOn;
+        animatorFinish = int.Parse(canvasFinish.text);
+        animatorAttack = canvasAttack.isOn;
+    }
+
+    void SetCanvasParametrs ()
+    {
+        canvasGround.isOn = animatorGround;
+        canvasWall.isOn = animatorWall;
+        canvasXSpeed.text = animatorXSpeed.ToString();
+        canvasYSpeed.text = animatorYSpeed.ToString();
+        canvasDead.isOn = animatorDead;
+        canvasShow.isOn = animatorShow;
+        canvasStartRound.isOn = animatorStartRound;
+        canvasFinish.text = animatorFinish.ToString();
+        canvasAttack.isOn = animatorAttack;
+    }
+
+    void SetAnimatorParametrs ()
+    {
+        //Значение для аниматора Body
+        animatorBody.SetBool("Ground", animatorGround);
+        animatorBody.SetBool("Wall", animatorWall);
+        animatorBody.SetFloat("xSpeed", animatorXSpeed);
+        animatorBody.SetFloat("ySpeed", animatorYSpeed);
+        animatorBody.SetBool("Dead", animatorDead);
+        animatorBody.SetBool("Show", animatorShow);
+        animatorBody.SetBool("StartRound", animatorStartRound);
+        animatorBody.SetInteger("Finish", animatorFinish);
+        animatorBody.SetBool("Attack", animatorAttack);
+
+        //Значение для аниматора Soul
+        animatorSoul.SetBool("Ground", animatorGround);
+        animatorSoul.SetBool("Wall", animatorWall);
+        animatorSoul.SetFloat("xSpeed", animatorXSpeed);
+        animatorSoul.SetFloat("ySpeed", animatorYSpeed);
+        animatorSoul.SetBool("Dead", animatorDead);
+        animatorSoul.SetBool("Show", !animatorShow);
+        animatorSoul.SetBool("StartRound", animatorStartRound);
+        animatorSoul.SetInteger("Finish", animatorFinish);
+        animatorSoul.SetBool("Attack", animatorAttack);
+    }
+}
