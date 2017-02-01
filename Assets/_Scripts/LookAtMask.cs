dddc1a69903a65c15ae2c0450dd8e95bfc2a7985
DESKTOP-2J8J5QE\Bridge <darkirius@gmail.com>
2017-02-02 00:01:35 +0300

    небольшое движение маски

--- Assets/_Scripts/LookAtMask.cs
+++ Assets/_Scripts/LookAtMask.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class LookAtMask : MonoBehaviour
 {
     public Transform target;
+    private Vector3 offset = new Vector3(-1,1,0);
 
-	void Update ()
+    void Update ()
     {
-        this.transform.LookAt(this.transform.position + Vector3.forward);
-        this.transform.position = Vector3.MoveTowards(this.transform.position, target.position + new Vector3(-1,1,0), Time.deltaTime);
+        this.transform.position = Vector3.MoveTowards(this.transform.position, target.position + offset, Time.deltaTime);
     }
 }
